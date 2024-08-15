7cf395d7cc714ca4353cc752be1143e187f4cf87
aa13xx <130700198+aa13xx@users.noreply.github.com>
2024-08-15 21:17:37 +0100

    v1.1.3

--- optimise.py
+++ optimise.py
@@ -24,7 +24,6 @@ irradiation_time = 1000 #s
 initial_guess = [beam_current, cooling_time, counting_time, distance_foil, irradiation_time]
 
 #this is the main objective function which takes in values and spit out the peak area of an interested peak energy
-
 def objective(parameters):
     #parameters 
     beam_current = parameters[0]
@@ -32,15 +31,16 @@ def objective(parameters):
     counting_time = parameters[2]
     distance_foil = parameters[3]
     irradiation_time = parameters[4]
-    
-    peak_energy = 343.4 #interested peak (in keV)
     #fixed
     proton_energy = 2e7 #eV
     thickness_foil = 0.01 #mm
+    #interested peak
+    peak_energy = 343.4 #keV
     
     peak_area = ta181_foil_process(beam_current, cooling_time, counting_time, distance_foil, irradiation_time, proton_energy, thickness_foil, peak_energy)
 
-    return -peak_area
+    return(-peak_area)
+
 #constraints
 def constraints1(parameters):
     #parameters
@@ -53,16 +53,16 @@ def constraints1(parameters):
     proton_energy = 2e7 #eV
     thickness_foil = 0.01 #mm
     #constraint values
-    foil_activity = 1e12 #in keV
+    foil_activity = 1e11 #keV
     x = ta181_foil_activity(beam_current, cooling_time, counting_time, distance_foil, irradiation_time, proton_energy, thickness_foil) - foil_activity
      
     return(x) #activity shouldnt exceed the preset foil_activity (radiation amount accumulated during gamma collection in keV)
 
-#constraint_1 = {"type": "ineq", "fun": constraints1}
+constraint_1 = {"type": "ineq", "fun": constraints1}
 
-#sol = minimize(objective, initial_guess, method = "SLSQP", bounds = boundary, constraints = constraint_1)
-#print(sol)
+sol = minimize(objective, initial_guess, method = "SLSQP", bounds = boundary, constraints = constraint_1)
+print(sol)
 
 #this is to test the process with the initial values
-test_initial = objective(initial_guess)
-print(-test_initial)
+#test_initial = objective(initial_guess)
+#print(-test_initial)
